c259e8e3b3981c0be7a038969217013654077346
Som Kamoji <somkamoji01@gmail.com>
2022-10-12 12:21:45 +0530

    finished build

---
+++ sanity-blog/schemas/comment.js
@@ -0,0 +1,33 @@
+export default {
+    name: "comment",
+    title: "Comment",
+    type: "document",
+    fields: [
+      {
+        name: "name",        
+        type: "string",
+      },
+      {
+        name: "approved",
+        title: "Approved",
+        type: "boolean",
+        description:"needed to show on site"
+      },
+      {
+        name: "email",        
+        type: "string",        
+      },
+      {
+        name: "comment",
+        type: "text",
+      },
+      {
+        name: "post",
+        type: "reference",
+        to:[{type:"post"}],
+      },
+    ],
+  
+   
+  };
+  
